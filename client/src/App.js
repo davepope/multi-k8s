8351b5bfeb1270d9a433a90a330623f969797d87
David Pope <epopevad@gmail.com>
2023-01-17 01:02:19 -0800

    Fib Calculator

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            <h1>Fib Calculator</h1>
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
