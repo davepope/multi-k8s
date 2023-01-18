447eeb5a51a9e4dbce67c9dfa1dd6f84e4141259
David Pope <epopevad@gmail.com>
2023-01-17 21:17:57 -0800

    fixed misconfig of kubectl set image

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ docker push epopevad/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=epopevad/multi-server:$SHA
-kubectl set image deployments/client-deployment server=epopevad/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=epopevad/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=epopevad/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=epopevad/multi-worker:$SHA
